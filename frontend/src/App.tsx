ca03acab5ec324c79c1bf85924090cf3baee7543
Lana H <141768307+lanah9@users.noreply.github.com>
2025-10-07 18:57:00 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,22 +4,124 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div class="grid-container">
+    <head>
+      <link rel="stylesheet" href="style.css" />
+      <title>Jet Piranha</title>
+    </head>
+    <body>
+      <div>
+        <header class="header">
+          <div class="brand">
+            <button onclick="openMenu()">&#9776;</button>
+            <a href="index.html">Jet Piranha</a>
+          </div>
+          <div class="header-links">
+            <a href="cart.html"> Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <aside class ="sidebar">
+          <h3>Shopping Categories</h3>
+          <button class = "sidebar-close-button" onclick ="closeMenu()">x</button>
+          <ul>
+            <li>
+              <a href="index.html">Pants</a>
+            </li>
+
+            <li>
+              <a href="index.html">Shirts</a>
+            </li>
+          </ul>
+
+        </aside>
+        <main>
+          <ul>
+            <li>Product 1</li>
+            <li>Product 2</li>
+            <li>Product 3</li>
+            <li>Product 4</li>
+            <li>Product 5</li>
+          </ul>
+          <div class="content">
+    <ul class="products">
+      
+<li>
+<div class="product">
+<img class="product-image" src="images/d1.jpg" alt="product" />
+
+  <div class="product-name">
+<a href="product.html">Slim Shirt</a>
+</div>
+<div class="product-brand">Nike</div>
+<div class="product-price">$60</div>
+<div class="product-rating">4.5 Stars (10 Reviews)</div>
+</div>
+</li>
+
+<li>
+<div class="product">
+<img class="product-image" src="images/d1.jpg" alt="product" />
+
+  <div class="product-name">
+<a href="product.html">Slim Shirt</a>
+</div>
+<div class="product-brand">Nike</div>
+<div class="product-price">$60</div>
+<div class="product-rating">4.5 Stars (10 Reviews)</div>
+</div>
+</li>
+
+<li>
+<div class="product">
+<img class="product-image" src="images/d1.jpg" alt="product" />
+
+  <div class="product-name">
+<a href="product.html">Slim Shirt</a>
+</div>
+<div class="product-brand">Nike</div>
+<div class="product-price">$60</div>
+<div class="product-rating">4.5 Stars (10 Reviews)</div>
+</div>
+</li>
+
+<li>
+<div class="product">
+<img class="product-image" src="images/d1.jpg" alt="product" />
+
+  <div class="product-name">
+<a href="product.html">Slim Shirt</a>
+</div>
+<div class="product-brand">Nike</div>
+<div class="product-price">$60</div>
+<div class="product-rating">4.5 Stars (10 Reviews)</div>
+</div>
+</li>
+
+<li>
+<div class="product">
+<img class="product-image" src="images/d1.jpg" alt="product" />
+
+  <div class="product-name">
+<a href="product.html">Slim Shirt</a>
+</div>
+<div class="product-brand">Nike</div>
+<div class="product-price">$60</div>
+<div class="product-rating">4.5 Stars (10 Reviews)</div>
+</div>
+</li>
+
+
+
+</ul>
+
+          </div>
+        </main>
+        <footer>&copy; 2021 Jet Piranha</footer>
+      </div>
+    </body>
+  </div>
   );
 }
 
