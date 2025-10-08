63cd65caecb56d5c28f96c941cf5581fea70c71c
Lana Ho <lanajho9@gmail.com>
2025-10-08 16:25:19 -0400

    create react app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,131 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const openMenu = () ⇒ {
-    document.querySelector(".sidebar")?.classList.add("open");
-}
-const closeMenu = () ⇒ {
-    document.querySelector(".sidebar")?.classList.remove("open");
-}
   return (
-
-      <div className="grid-container">
-    <head>
-      <link rel="stylesheet" href="style.css" />
-      <title>Jet Piranha</title>
-    </head>
-    <body>
-      <div>
-        <header className="header">
-          <div className="brand">
-            <button onClick={openMenu}>&#9776;</button>
-            <a href="index.html">Jet Piranha</a>
-          </div>
-          <div className="header-links">
-            <a href="cart.html"> Cart</a>
-            <a href="signin.html">Sign In</a>
-          </div>
-        </header>
-        <aside className ="sidebar">
-          <h3>Shopping Categories</h3>
-          <button className = "sidebar-close-button" onclick ={closeMenu}>x</button>
-          <ul>
-            <li>
-              <a href="index.html">Pants</a>
-            </li>
-
-            <li>
-              <a href="index.html">Shirts</a>
-            </li>
-          </ul>
-
-        </aside>
-        <main>
-          <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
-            <li>Product 4</li>
-            <li>Product 5</li>
-          </ul>
-          <div className="content">
-    <ul className="products">
-      
-<li>
-<div className="product">
-<img className="product-image" src="images/d1.jpg" alt="product" />
-
-  <div className="product-name">
-<a href="product.html">Slim Shirt</a>
-</div>
-<div className="product-brand">Nike</div>
-<div className="product-price">$60</div>
-<div className="product-rating">4.5 Stars (10 Reviews)</div>
-</div>
-</li>
-
-<li>
-<div className="product">
-<img className="product-image" src="images/d1.jpg" alt="product" />
-
-  <div className="product-name">
-<a href="product.html">Slim Shirt</a>
-</div>
-<div className="product-brand">Nike</div>
-<div className="product-price">$60</div>
-<div className="product-rating">4.5 Stars (10 Reviews)</div>
-</div>
-</li>
-
-<li>
-<div className="product">
-<img className="product-image" src="images/d1.jpg" alt="product" />
-
-  <div className="product-name">
-<a href="product.html">Slim Shirt</a>
-</div>
-<div className="product-brand">Nike</div>
-<div className="product-price">$60</div>
-<div className="product-rating">4.5 Stars (10 Reviews)</div>
-</div>
-</li>
-
-<li>
-<div className="product">
-<img className="product-image" src="images/d1.jpg" alt="product" />
-
-  <div className="product-name">
-<a href="product.html">Slim Shirt</a>
-</div>
-<div className="product-brand">Nike</div>
-<div className="product-price">$60</div>
-<div className="product-rating">4.5 Stars (10 Reviews)</div>
-</div>
-</li>
-
-<li>
-<div className="product">
-<img className="product-image" src="images/d1.jpg" alt="product" />
-
-  <div className="product-name">
-<a href="product.html">Slim Shirt</a>
-</div>
-<div className="product-brand">Nike</div>
-<div className="product-price">$60</div>
-<div className="product-rating">4.5 Stars (10 Reviews)</div>
-</div>
-</li>
-
-
-
-</ul>
-
-          </div>
-        </main>
-        <footer>&copy; 2021 Jet Piranha</footer>
-      </div>
-    </body>
-  </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
