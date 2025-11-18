d21482b3092a059c61be2c3b57258d76cecc973d
Lana Ho <lanajho9@gmail.com>
2025-11-18 15:03:58 -0500

    finish page 4 for lab 6

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,16 +1,42 @@
-import data from "./data";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+  brand: string;
+  price: number;
+  rating: number;
+  numberOfReviews: number;
+  imageUrl: string;
+};
+
+const emptyProducts: Product[] = [];
 
 function Products() {
+  const [products, setProducts]: [Product[], (products: Product[]) => void] =
+    useState(emptyProducts);
+
+  useEffect(() => {
+    axios
+      .get<Product[]>("https://localhost:7250/catalog", {
+        headers: { "Content-Type": "application/json" },
+      })
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="content">
       <ul className="products">
-        {data.products.map((product) => (
-          <li>
+        {products.map((product) => (
+          <li key={product.id || product.name}>
             <div className="product">
               <img
                 className="product-image"
                 src={product.imageUrl}
-                alt="product"
+                alt={product.name}
               />
               <div className="product-name">
                 <a href="product.html">{product.name}</a>
@@ -23,9 +49,9 @@ function Products() {
             </div>
           </li>
         ))}
-        ;
       </ul>
     </div>
   );
 }
+
 export default Products;
